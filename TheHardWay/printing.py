9caa7359d5b02a91fe9f71872d06df5a6bdee6f2
Dan Barnes <barnzdan@gmail.com>
2012-02-06 21:32:36 -0800

    printing python practice

---
+++ TheHardWay/printing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, True, False)
+print formatter % (formatter, formatter, formatter, formatter)
