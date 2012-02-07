73f5601b45a19972b64cfe2d22e3475351d561fc
Dan Barnes <barnzdan@gmail.com>
2012-02-06 21:24:16 -0800

    created directory for THW series on python

---
+++ TheHardWay/format_strings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+my_name = 'Dan L. Barnes'
+my_age = 34
+my_height = 68 # inches
+my_weight = 164 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d, and %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight )
