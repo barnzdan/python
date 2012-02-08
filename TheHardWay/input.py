7a8e19a382e8c733a9a0e3daf31e5e6f56f9acb5
Dan Barnes <barnzdan@gmail.com>
2012-02-07 16:00:48 -0800

    added the hard way series

---
+++ TheHardWay/input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+print "How old are you",
+age = raw_input()
+print "How tall are you",
+height = raw_input()
+print "How much do you weigh",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
---
+++ TheHardWay/tabs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Catfood
+\t* Fishies 
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
