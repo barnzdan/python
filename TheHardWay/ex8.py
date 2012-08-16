cf2b95ea0eda883965596f86048ad3b716fc1b85
Dan Barnes <barnzdan@gmail.com>
2012-08-15 17:59:08 -0700

    added excercise 8

---
+++ TheHardWay/ex8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
