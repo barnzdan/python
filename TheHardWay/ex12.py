884452d0f78e12897de89a0106da2ff5e991115d
Dan Barnes <barnzdan@gmail.com>
2012-08-15 18:43:33 -0700

    stuff

---
+++ TheHardWay/ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So you're %s old, %s tall and %s heavy." % (
+    age, height, weight )
---
+++ TheHardWay/ex13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", first
+print "Your third variable is:", first
