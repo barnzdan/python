15b65a318e8c9c22b65d81c595264dff970b1d8e
ajh1143 <ajh1143@users.noreply.github.com>
2017-07-09 03:33:57 -0700

    Update myclass.py

--- OOP/myclass.py
+++ OOP/myclass.py
@@ -16,9 +16,9 @@ class myClass():
     
     #bmi calculator and printer
     def bmi(self):
-    conversion = (self.height/10) + (4/12)
-    bmimath = (conversion*4.88/self.height*self.height)
-    print("{}, your BMI is: {}".format(self.username, bmimath))
+        conversion = (self.height/10) + (4/12)
+        bmimath = (conversion*4.88/self.height*self.height)
+        print("{}, your BMI is: {}".format(self.username, bmimath))
     
 
 
